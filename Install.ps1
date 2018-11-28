91e890b3413fba53e1601c39553d51f4e995d984
ZTI <zhaotianff@163.com>
2018-11-28 21:14:20 +0800

    update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,14 +1,27 @@
-﻿#check network connection
+﻿#register lib
+regsvr32 /s .\APlayer.dll
+regsvr32 /s .\APlayerUI.dll
+
+#check network connection
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$DecodePackUrl = "https://technet-info.com/ZtiPlayer/codecs.zip"
+$TempPath = $env:TEMP
+$CodecsFileName = "codecs.zip"
+$FullPath = "$TempPath\$CodecsFileName"
+
 $testIP = [System.Net.Dns]::GetHostEntry("technet-info.com").AddressList[0].ToString();
 $pingResult = Test-NetConnection $testIP
 if($pingResult.PingSucceeded -eq $true)
 {
-    "True"
+    $client = New-Object System.Net.WebClient
+    "Downloading decode pack,please wait......."
+    $client.DownloadFile($DecodePackUrl,$FullPath)
+
 }
 else
 {
-    "False"
+    [System.Windows.Forms]::MessageBox.Show("Check you network connection")
     retrun;
 }
-
-"HelloWorld"
