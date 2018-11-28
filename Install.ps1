477b564b343d8fb0a27a8d35251be15f70f547a3
ZTI <zhaotianff@163.com>
2018-11-28 17:36:52 +0800

    add install script

---
+++ Install.ps1
@@ -0,0 +1,14 @@
+﻿#check network connection
+$testIP = [System.Net.Dns]::GetHostEntry("technet-info.com").AddressList[0].ToString();
+$pingResult = Test-NetConnection $testIP
+if($pingResult.PingSucceeded -eq $true)
+{
+    "True"
+}
+else
+{
+    "False"
+    retrun;
+}
+
+"HelloWorld"
