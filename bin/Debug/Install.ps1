e17d046dd74f7f1d6149bfadd1716b2d19aa8822
ZTI <zhaotianff@163.com>
2021-01-12 16:15:00 +0800

    update Install.ps1

--- bin/Debug/Install.ps1
+++ bin/Debug/Install.ps1
@@ -8,19 +8,55 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.IO.Compression
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
-$DecodePackUrl = "https://technet-info.com/ZtiPlayer/codecs.zip"
+$DecodePackUrl = "http://aplayer.open.xunlei.com/codecs.zip"
 $TempPath = $env:TEMP
 $CodecsFileName = "codecs.zip"
 $FullPath = "$TempPath\$CodecsFileName"
 
-$testIP = [System.Net.Dns]::GetHostEntry("technet-info.com").AddressList[0].ToString();
+$CodecsExtractName = "codecs"
+$CodecsExtractPath = "$TempPath\$CodecsExtractName"
+
+$thumderPath = [System.Environment]::GetFolderPath('CommonDesktopDirectory').Replace("\Desktop","") + ""
+
+$testIP = [System.Net.Dns]::GetHostEntry("aplayer.open.xunlei.com").AddressList[0].ToString();
 $pingResult = Test-NetConnection $testIP
 if($pingResult.PingSucceeded -eq $true)
 {
     $client = New-Object System.Net.WebClient
     "Downloading decode pack,please wait......."
-    $client.DownloadFile($DecodePackUrl,$FullPath)
-    [System.IO.Compression]::ZipFile.ExtractToDirectory("D:\\a.zip", "D:\\"); #temp
+
+    $existDecodePack = Test-Path $FullPath
+    if($existDecodePack -eq $false)
+    {
+        $client.DownloadFile($DecodePackUrl,$FullPath)
+    }
+    "Extracting decode pack......"
+
+    $existExtractDir = Test-Path $CodecsExtractPath
+    if($existExtractDir -eq $false)
+    {
+        md $CodecsExtractPath
+    }
+
+
+    #temp
+    $count = (ls $CodecsExtractPath).Count
+
+    if($count -eq 0)
+    {
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($FullPath, $CodecsExtractPath);
+        "Extract codec file success."
+    }
+
+    #copy file 
+    $existThumderDir = Test-Path $thumderPath
+    if($existThumderDir -eq $false)
+    {
+        md $thumderPath
+    }
+
+    copy -Path $CodecsExtractPath -Destination $thumderPath -Recurse
+
     "Press any key to exit"
     Read-Host
 }
